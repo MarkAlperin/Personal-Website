54f9d5b730f747c4745f0110cd5e185c7126d7c4
MarkAlperin <mark.t.alperin@gmail.com>
2022-07-05 17:23:47 -0500

    add styling to SkillCard

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SkillCard = () => {
+const SkillCard = (props) => {
   return (
-    <div></div>
+    <SkillCardContainer>
+      <IconContainer/>
+    </SkillCardContainer>
   )
 };
 
 export default SkillCard;
+
+const SkillCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  dropshadow: 5px 5px 5px black;
+  `;
+
+  const IconContainer = styled.div``;

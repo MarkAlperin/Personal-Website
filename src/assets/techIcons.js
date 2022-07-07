ef83ec408bb374e1d86fa638db76abcbcba9a4d9
MarkAlperin <mark.t.alperin@gmail.com>
2022-07-07 09:27:30 -0500

    changing techIcons to an array

--- src/assets/techIcons.js
+++ src/assets/techIcons.js
@@ -1,150 +1,149 @@
-const icons = {
-  AWS: {
+const icons = [
+  {
     href: "https://aws.amazon.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
-    alt: "Amazon Web Services",
+    alt: "AWS",
   },
-  CSS3: {
+  {
     href: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
     alt: "CSS3",
   },
-  Express: {
+  {
     href: "https://expressjs.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/expressjs/expressjs-original-wordmark.svg",
     alt: "Express",
   },
-  Jest: {
+  {
     href: "https://jestjs.io",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-original-wordmark.svg",
     alt: "Jest",
   },
-  Mocha: {
+  {
     href: "https://mochajs.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mocha/mocha-original-wordmark.svg",
     alt: "Mocha",
   },
-  Chai: {
+  {
     href: "https://chaijs.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/chai/chai-original-wordmark.svg",
     alt: "Chai",
   },
-  Node: {
+  {
     href: "https://nodejs.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
     alt: "Node",
   },
-  React: {
+  {
     href: "https://reactjs.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
     alt: "React",
   },
-  Redux: {
+  {
     href: "https://redux.js.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original-wordmark.svg",
     alt: "Redux",
   },
-  MongoDB: {
+  {
     href: "https://www.mongodb.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
     alt: "MongoDB",
   },
-  PostgreSQL: {
+  {
     href: "https://www.postgresql.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
     alt: "PostgreSQL",
   },
-  Sequelize: {
+  {
     href: "https://sequelize.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sequelize/sequelize-original-wordmark.svg",
     alt: "Sequelize",
   },
-  MySQL: {
+  {
     href: "https://www.mysql.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
     alt: "MySQL",
   },
-  Mongoose: {
+  {
     href: "https://mongoosejs.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongoose/mongoose-original-wordmark.svg",
     alt: "Mongoose",
   },
-  Nginx: {
+  {
     href: "https://nginx.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original-wordmark.svg",
     alt: "Nginx",
   },
-  SQLite: {
+  {
     href: "https://www.sqlite.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sqlite/sqlite-original-wordmark.svg",
     alt: "SQLite",
   },
-  Webpack: {
+  {
     href: "https://webpack.js.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/webpack/webpack-original-wordmark.svg",
     alt: "Webpack",
   },
-  JavaScript: {
+  {
     href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original-wordmark.svg",
     alt: "JavaScript",
   },
-  TypeScript: {
+  {
     href: "https://www.typescriptlang.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original-wordmark.svg",
     alt: "TypeScript",
   },
-  HTML5: {
+  {
     href: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
     alt: "HTML5",
   },
-  Babel: {
+  {
     href: "https://babeljs.io",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/babel/babel-original-wordmark.svg",
     alt: "Babel",
   },
-  Bash: {
+  {
     href: "https://www.gnu.org/software/bash/",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bash/bash-original-wordmark.svg",
     alt: "Bash",
   },
-  Bootstrap: {
+  {
     href: "https://getbootstrap.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original-wordmark.svg",
     alt: "Bootstrap",
   },
-  Firebase: {
+  {
     href: "https://firebase.google.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-original-wordmark.svg",
     alt: "Firebase",
   },
-  Heroku: {
+  {
     href: "https://www.heroku.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/heroku/heroku-original-wordmark.svg",
     alt: "Heroku",
   },
-  Python: {
+  {
     href: "https://www.python.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
     alt: "Python",
   },
-  Pandas: {
+  {
     href: "https://pandas.pydata.org",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original-wordmark.svg",
     alt: "Pandas",
   },
-  Postman: {
+  {
     href: "https://www.getpostman.com",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postman/postman-original-wordmark.svg",
     alt: "Postman",
   },
-  ReactNative: {
+  {
     href: "https://facebook.github.io/react-native/",
     src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react-native/react-native-original-wordmark.svg",
     alt: "React Native",
   },
-
-  };
+];
 
 export default icons;

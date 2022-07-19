1c67dbfa6e12aa340ffb149acda9ac1aa7d2ef2d
MarkAlperin <mark.t.alperin@gmail.com>
2022-07-19 17:07:37 -0500

    reorganized techIcons.js for tech stack categories

--- src/assets/techIcons.js
+++ src/assets/techIcons.js
@@ -1,6 +1,11 @@
 const icons = {
   front: [
     {
+      href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+      alt: "JavaScript",
+    },
+    {
       href: "https://www.typescriptlang.org",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
       alt: "TypeScript",
@@ -11,14 +16,34 @@ const icons = {
       alt: "React",
     },
     {
+      href: "https://facebook.github.io/react-native/",
+      src: "https://reactnative.dev/img/header_logo.svg",
+      alt: "React Native",
+    },
+    {
       href: "https://redux.js.org",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
       alt: "Redux",
     },
     {
+      href: "https://babeljs.io",
+      src: "https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg",
+      alt: "Babel",
+    },
+    {
       href: "https://webpack.js.org",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/webpack/webpack-original-wordmark.svg",
       alt: "Webpack",
+    },
+    {
+      href: "https://getbootstrap.com",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original-wordmark.svg",
+      alt: "Bootstrap",
+    },
+    {
+      href: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+      alt: "HTML5",
     },
     {
       href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
@@ -37,6 +62,51 @@ const icons = {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
       alt: "Express",
     },
+    {
+      href: "https://www.python.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
+      alt: "Python",
+    },
+    {
+      href: "https://pandas.pydata.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original-wordmark.svg",
+      alt: "Pandas",
+    },
+    {
+      href: "https://www.mongodb.com",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      alt: "MongoDB",
+    },
+    {
+      href: "https://www.postgresql.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+      alt: "PostgreSQL",
+    },
+    {
+      href: "https://www.mysql.com",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      alt: "MySQL",
+    },
+    {
+      href: "https://www.sqlite.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sqlite/sqlite-original-wordmark.svg",
+      alt: "SQLite",
+    },
+    {
+      href: "https://nginx.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg",
+      alt: "Nginx",
+    },
+    {
+      href: "https://sequelize.org",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sequelize/sequelize-original-wordmark.svg",
+      alt: "Sequelize",
+    },
+    {
+      href: "https://www.getpostman.com",
+      src: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      alt: "Postman",
+    },
   ],
   development: [
     {
@@ -44,8 +114,16 @@ const icons = {
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
       alt: "AWS",
     },
-  ],
-  skills: [
+    {
+      href: "https://firebase.google.com/",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-original-wordmark.svg",
+      alt: "Firebase",
+    },
+    {
+      href: "https://www.heroku.com",
+      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/heroku/heroku-original-wordmark.svg",
+      alt: "Heroku",
+    },
     {
       href: "https://jestjs.io",
       src: "https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg",
@@ -61,94 +139,10 @@ const icons = {
       src: "https://www.vectorlogo.zone/logos/chaijs/chaijs-icon.svg",
       alt: "Chai",
     },
-
-
-    {
-      href: "https://www.mongodb.com",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
-      alt: "MongoDB",
-    },
-    {
-      href: "https://www.postgresql.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
-      alt: "PostgreSQL",
-    },
-    {
-      href: "https://sequelize.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sequelize/sequelize-original-wordmark.svg",
-      alt: "Sequelize",
-    },
-    {
-      href: "https://www.mysql.com",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-      alt: "MySQL",
-    },
-    {
-      href: "https://nginx.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg",
-      alt: "Nginx",
-    },
-    {
-      href: "https://www.sqlite.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sqlite/sqlite-original-wordmark.svg",
-      alt: "SQLite",
-    },
-
-    {
-      href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-      alt: "JavaScript",
-    },
-
-    {
-      href: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-      alt: "HTML5",
-    },
-    {
-      href: "https://babeljs.io",
-      src: "https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg",
-      alt: "Babel",
-    },
     {
       href: "https://www.gnu.org/software/bash/",
       src: "https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg",
       alt: "Bash",
-    },
-    {
-      href: "https://getbootstrap.com",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original-wordmark.svg",
-      alt: "Bootstrap",
-    },
-    {
-      href: "https://firebase.google.com",
-      src: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
-      alt: "Firebase",
-    },
-    {
-      href: "https://www.heroku.com",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/heroku/heroku-original-wordmark.svg",
-      alt: "Heroku",
-    },
-    {
-      href: "https://www.python.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
-      alt: "Python",
-    },
-    {
-      href: "https://pandas.pydata.org",
-      src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original-wordmark.svg",
-      alt: "Pandas",
-    },
-    {
-      href: "https://www.getpostman.com",
-      src: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
-      alt: "Postman",
-    },
-    {
-      href: "https://facebook.github.io/react-native/",
-      src: "https://reactnative.dev/img/header_logo.svg",
-      alt: "React Native",
     },
   ],
 };

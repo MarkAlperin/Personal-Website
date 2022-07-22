5b31d1782ba0b4ead309f814d2c51514c252299a
MarkAlperin <mark.t.alperin@gmail.com>
2022-07-22 12:11:35 -0700

    restore App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import React from "react";
 import FrontPage from "./components/displays/FrontPage";
 import { AppContextProvider } from "./context/appContext";
 
+
 const App = () => {
   return (
     <AppContextProvider>
       <FrontPage />
     </AppContextProvider>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<FrontPage />} />
-    //     <Route path="/resume" element={<Resume />} />
-    //   </Routes>
-    // </BrowserRouter>
   );
+
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <AppContextProvider>
+  //         <Route path="/" element={<FrontPage />} />
+  //       </AppContextProvider>
+  //       <Route path="/cradleMountain" element={<CradleMountainApp />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 };
 
 export default App;

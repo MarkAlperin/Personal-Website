4fb740150e3061aac67f298c44df30a0bed88741
MarkAlperin <mark.t.alperin@gmail.com>
2022-08-18 17:04:10 -0500

    build reservations route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { AppContextProvider } from "./context/appContext";
 import FrontPage from "./components/displays/FrontPage";
 // import Fallback from "./components/Fallback";
 const TennisTime = React.lazy(() => import("./apps/tennisTime/TennisTime"));
-
+const ReservationsDisplay = React.lazy(() => import("./apps/tennisTime/ReservationsDisplay"));
 
 function App() {
   return (
@@ -13,12 +13,27 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<AppContextProvider>
-            <FrontPage />
-          </AppContextProvider>} />
+          element={
+            <AppContextProvider>
+              <FrontPage />
+            </AppContextProvider>
+          }
+        />
         <Route
           path="/tennis-time"
-          element={<Suspense fallback={<div>Loading...</div>}><TennisTime /></Suspense>} />
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <TennisTime />
+            </Suspense>
+          }
+        />
+        <Route path="/tennis-time/reservations"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ReservationsDisplay />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
